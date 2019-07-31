1ffc845b7e39b89c5897346493fdeb956e90ecb5
berkayaskin <berkayaskiin@gmail.com>
2019-07-31 22:48:47 +0300

    folder structure reorganized, directory and menu-item components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 const App = () => {
     return (
         <div className="App">
-          <HomePage />
+            <HomePage/>
         </div>
     );
 }
